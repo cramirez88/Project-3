718d16ebd0fa1f1662272b44dab794c653d4c8d9
Christian Ramirez <christian.r0602@gmail.com>
2020-12-14 21:42:57 -0600

    connecting new db to atlas

--- server.js
+++ server.js
@@ -41,7 +41,14 @@ app.use(cors());
 
 
  
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/pickup02")
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/pickup02"),
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './client/build')));
